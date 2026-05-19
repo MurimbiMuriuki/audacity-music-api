a5170434252cca27b7e6e6a2d19ce558bcca2c74
ParveenAcewebX <parveen@acewebx.com>
2026-05-19 19:39:04 +0530

    Fix some issue

--- config/config.js
+++ config/config.js
@@ -6,7 +6,8 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: "mysql",
-    port: process.env.DB_PORT,
+    port: process.env.DB_PORT
+  },
   test: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
@@ -23,4 +24,4 @@ module.exports = {
     dialect: "mysql",
     port: process.env.DB_PORT,
   },
-}}
+}
